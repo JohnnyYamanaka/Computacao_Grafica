a6b69b0dda932e83ff1b0893f17b58a0b51c6d63
lucastgama <lucas_paraty756@hotmail.com>
2019-06-11 13:05:35 -0300

    esta aqui a mudança da camera e obj solidos

--- projetoFinal.c
+++ projetoFinal.c
@@ -4,45 +4,38 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+//=============================================================
 static int year = 0, day = 0, days = 0;
 int tempo = 150;
-
 float GL_PI = 3.14;
-
 //variáveis que determinam o raio
 float gravidade = 5.0;
 float gravidadeGreen = 5.5;
-
 //velocidade de atração do raio
 float atracao = 0.1;
-
 float radiusNeutronPlanet = 0.2;
 float radiusNormalPlanet = 1.0;
 bool isRadiusPositive = true;
 
-void init(void) {
+
+void init(void) 
+{
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glShadeModel(GL_FLAT);
 }
 
-void reshape(int w, int h){
-	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt (0.0, 7.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // 0.0, 7.0, 5.0 | 0.0, 0.0, 15.0
-}
+
 
 //desenha um planeta vermelha que gira em torno do centro
 void redPlanet(void){
 	if(gravidade >= 0 & isRadiusPositive == true){
+		glLineWidth(2.5);
 		glColor3f(1.0, 0.0, 0.0); //vermelho
 		glPushMatrix();
 			glRotatef((GLfloat) day, 0.0, 10.0, 45.0); //rotaciona o objeto no seu próprio eixo.
 			glTranslatef(gravidade * 2, 0.0, 0.0); //rotaciona o objeto num eixo determinado
-			glutWireSphere(radiusNormalPlanet, 15.0, 8.0);
+			glutSolidSphere(radiusNormalPlanet, 15.0, 8.0);
 		glPopMatrix();		
 	} else{
 		gravidade = 0;
@@ -55,9 +48,9 @@ void greenPlanet(void){
 	if(gravidadeGreen >= 0 & isRadiusPositive == true){
 		glColor3f(0.3, 1.0, 0.1); //verde
 		glPushMatrix();
-			glRotatef((GLfloat) days, 0.0, 0.0, 45.0); //rotaciona o objeto no seu próprio eixo.
+			glRotatef((GLfloat) days, 1.0, 0.0, 45.0); //rotaciona o objeto no seu próprio eixo.
 			glTranslatef(gravidadeGreen, 0.0, 0.0); //rotaciona o objeto num eixo determinado
-			glutWireSphere(radiusNormalPlanet, 15.0, 8.0);
+			glutSolidSphere(radiusNormalPlanet, 15.0, 8.0);
 		glPopMatrix();		
 	} else{
 		gravidade = 0;
@@ -90,17 +83,19 @@ void neutronMerge(void){
 		glPushMatrix();
 			glRotatef((GLfloat) days * 2, 0.0, 0.0, 90.0); //rotaciona o objeto no seu próprio eixo.
 			glTranslatef(0.05, 0.0, 0.0); //rotaciona o objeto num eixo determinado
-			glutWireSphere(radiusNeutronPlanet, 30.0, 8.0);
+			glutSolidSphere(radiusNeutronPlanet, 30.0, 8.0);
 		glPopMatrix();		
 	}
 }
 
-void starryBackground(void){
+void starryBackground(void)
+{
 	int size = 500;
 	float xPosition[size], yPosition[size], zPosition[size];
 	float bagckgroundStarPosition [50][50][50];
 
-	for(int i = 0; i < size + 1; i++){
+/*	for(int i = 0; i < size + 1; i++)
+	{
 		float upper = 10.0;
 		float lower = -10.0;
 
@@ -111,11 +106,8 @@ void starryBackground(void){
 		xPosition[i] = xrandomNumber;
 		yPosition[i] = yrandomNumber;
 		zPosition[i] = zrandomNumber;
-
-		printf("xPosition %.2f\n", xPosition[i]);
-		printf("yPosition %.2f\n", yPosition[i]);
-		printf("zPosition %.2f\n", zPosition[i]);
-	}
+		
+	}*/
 
 	int index = 0;
 	for (int index = 0; index < size; index++){
@@ -136,6 +128,7 @@ void display(void){
 //	orbit();
 	redPlanet();
  	glutSwapBuffers();
+ 	
 }
 
 void animacao(int x){
@@ -156,22 +149,40 @@ void animacao(int x){
 	if(radiusNeutronPlanet >= 2.5){
 		radiusNeutronPlanet = 2.5;
 	}
-	printf("%.2f\n", gravidade);
 	display();
 	glutTimerFunc(tempo, animacao, 0);
 }
 
-void main(int argc, char** argv){
+void reshape(int w, int h){
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+	GLfloat b =(GLfloat) w/(GLfloat)h;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+    gluPerspective (16, b, 1, 800);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt (0.0, 30.0, 50.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	
+}
+
+//FIM DE FUNCAO
+//============================================================================//
+
+
+
+
+//============================================================================//
+
+int main(int argc, char** argv)
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(1000, 500);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(50, 50);
 	glutCreateWindow("Neutron Merge");
 	init();
-	
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
-	//glutKeyboardFunc(keyboard);
 	glutTimerFunc(tempo, animacao, 0);
 	glutMainLoop();
 }
